4eeef7b12e20364e30d46be582665f5fae93b988
Shubham k <aryakumar925@gmail.com>
2024-02-17 19:58:55 +0530

    fix buges

--- lib/modules/main_screen/controller/home_controller.dart
+++ lib/modules/main_screen/controller/home_controller.dart
@@ -153,6 +153,7 @@ class HomeController extends GetxController{
               message: 'Something Went Wrong.',
               color: CustomTheme.errorColor);
               */
+          break;
           log('Something Went Wrong.');
         }
       };
